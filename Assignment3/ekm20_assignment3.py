91e66478fd84c72ad3db9e664ed32f955185d71e
ekmill <ekm20@uw.edu>
2022-05-05 11:29:19 -0700

    Assignment 3 part 2.1 finished, things have been reorganizedinto predictsimplistic

--- Assignment3/ekm20_assignment3.py
+++ Assignment3/ekm20_assignment3.py
@@ -38,23 +38,16 @@ NEG = 'bad'
 def predict_simplistic(f):
     with open(f, 'r') as f:
         text = f.read()
-
-    # The below line will clean the text of punctuation marks.
     clean_text = text.translate(str.maketrans('', '', string.punctuation))
-    return clean_text
-
-    # replaces all tabs with spaces, and also all occurrences of more than one space in a row with a single space.
-    cleaner_text = re.sub('\s+', ' ', text)
-    return cleaner_text
+    clean_text = re.sub('\s+', ' ', text)
     token_counts = {}
-    tokens = clean_text.split()
+    tokens = cleaner_text.split()
     word_value = 0
     for word in tokens:
         if word not in token_counts:
             token_counts[word] = word_value
         elif word in token_counts:
             token_counts[word] += 1
-    return token_counts
     pos_count = token_counts.get(POS, 0)
     neg_count = token_counts.get(NEG, 0)
     if pos_count > neg_count:
@@ -74,16 +67,9 @@ def main(argv):
     # The file that you will read should be passed as the argument to the program.
     filename = argv[1]
 
-    # Now, we will call cleanFileContents on the filename we were passed.
-    clean_text = clean_file_contents(filename)
-
-    # Now, we will count how many times each word occurs in the review.
-    # We assign the output of the function to a new variable we call tokens_with_counts.
-    tokens_with_counts = count_tokens(clean_text)
-
     # Call the simplistic prediction function on the obtained counts.
     # Store the output of the function in a new variable called "prediction".
-    prediction = predict_simplistic(tokens_with_counts)
+    prediction = predict_simplistic(f)
 
     # Finally, let's print out what we predicted.
     print("The prediction for file {} is {}".format(filename, prediction))
