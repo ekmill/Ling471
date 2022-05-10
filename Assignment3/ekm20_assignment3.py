e6d5eb4434b464a7cab594fff9c5183cfc98a4c6
ekmill <ekm20@uw.edu>
2022-05-09 19:16:38 -0700

    gets counts

--- Assignment3/ekm20_assignment3.py
+++ Assignment3/ekm20_assignment3.py
@@ -31,6 +31,10 @@ NEG = 'bad'
 
 # this method retrieves counts of good and bad, and returns what type of review it is.
 def predict_simplistic(dir):
+    true_pos = 0
+    true_neg = 0
+    false_pos = 0
+    false_neg = 0
     pos = Path('/Users/emma/Downloads/aclImdb/train/pos')
     neg = Path('/Users/emma/Downloads/aclImdb/train/neg')
     pos_file = [f for f in pos.glob('*') if f.is_file]
@@ -51,13 +55,16 @@ def predict_simplistic(dir):
         pos_count = token_counts.get(POS, 0)
         neg_count = token_counts.get(NEG, 0)
         if pos_count > neg_count:
-            print("The prediction for file {} is POSITIVE".format(filename))
+           # print("The prediction for file {} is POSITIVE".format(filename))
+            true_pos += 1
         elif neg_count > pos_count:
-            print("The prediction for file {} is NEGATIVE".format(filename))
-        else:
-            print("The prediction for file {} is NONE".format(filename))
+           # print("The prediction for file {} is NEGATIVE".format(filename))
+            false_neg += 1
+       # else:
+           # print("The prediction for file {} is NONE".format(filename))
+         #   false_neg += 1
     for filename in neg_file:
-        with open(filename, 'r') as f:
+        with open(filename, 'r', encoding='utf-8') as f:
             text = f.read()
         clean_text = text.translate(str.maketrans('', '', string.punctuation))
         clean_text = re.sub('\s+', ' ', clean_text)
@@ -72,14 +79,18 @@ def predict_simplistic(dir):
         pos_count = token_counts.get(POS, 0)
         neg_count = token_counts.get(NEG, 0)
         if pos_count > neg_count:
-            print("The prediction for file {} is POSITIVE".format(filename))
+           # print("The prediction for file {} is POSITIVE".format(filename))
+            false_pos += 1
         elif neg_count > pos_count:
-            print("The prediction for file {} is NEGATIVE".format(filename))
-        else:
-            print("The prediction for file {} is NONE".format(filename))
-
+           # print("The prediction for file {} is NEGATIVE".format(filename))
+            true_neg += 1
+       # else:
+           # print("The prediction for file {} is NONE".format(filename))
+          #  false_pos += 1
+    print(true_pos, true_neg, false_pos, false_neg)
 
 def main(argv):
+    filename = argv[1]
     # The file that you will read should be passed as the argument to the program.
     prediction = predict_simplistic(dir)
 
