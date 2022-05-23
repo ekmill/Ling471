8aad4590b15521aee8e98433f5947409d6eb7406
ekmill <ekm20@uw.edu>
2022-05-23 08:38:51 -0700

    all in one function, reviews retrieved

--- Assignment 4/imdbdataframe.py
+++ Assignment 4/imdbdataframe.py
@@ -6,56 +6,55 @@ import re
 from pathlib import Path
 
 
-train_pos = Path('/Users/emma/Downloads/aclImdb/train/pos')
-train_neg = Path('/Users/emma/Downloads/aclImdb/train/neg')
-test_pos = Path('/Users/emma/Downloads/aclImdb/test/pos')
-test_neg = Path('/Users/emma/Downloads/aclImdb/test/neg')
-
-def directory_list(argv):
-    my_list = argv[1]
-    my_list [f for f in train_pos.glob('*') if f.is_file]
-    argv[2] = [f for f in train_neg.glob('*') if f.is_file]
-    argv[3] = [f for f in test_pos.glob('*') if f.is_file]
-    argv[4] = [f for f in test_neg.glob('*') if f.is_file]
-    return argv[1: 4]
+pos_train = Path('/Users/emma/Downloads/aclImdb/train/pos')
+neg_train = Path('/Users/emma/Downloads/aclImdb/train/neg')
+pos_test = Path('/Users/emma/Downloads/aclImdb/test/pos')
+neg_test = Path('/Users/emma/Downloads/aclImdb/test/neg')
 
 # Constants:
 POS = 1
 NEG = 0
-n = 0
+
 
 def createDataFrame(argv):
+    pos_train_files = [f for f in pos_train.glob('*') if f.is_file]
+    neg_train_files = [f for f in neg_train.glob('*') if f.is_file]
+    pos_test_files = [f for f in pos_test.glob('*') if f.is_file]
+    neg_test_files = [f for f in neg_test.glob('*') if f.is_file]
     new_filename = "my_imdb_dataframe.csv"
     column_names = ["file", "label", "type", "review"]
-    df = pd.DataFrame(data=argv[1:4], columns=column_names)
-    for filename in directory_list(argv):
-        with open(filename, 'r') as f:
+    df = pd.DataFrame(columns=column_names)
+    for filename in pos_train_files:
+        with open(filename, 'r', encoding='utf-8') as f:
+            file = filename
+            review = cleanFileContents(f)
+            label = 'pos'
+            type = 'train'
+            df.loc[len(df.index)] = [file, label, type, review]
+    for filename in neg_train_files:
+        with open(filename, 'r', encoding='utf-8') as f:
             file = filename
             text = f.read()
             review = cleanFileContents(text)
-            if 'pos' in Path:
-                label = 'pos'
-            else:
-                label = 'neg'
-            if 'train' in Path:
-                type = 'train'
-            else:
-                type = 'test'
+            label = 'neg'
+            type = 'train'
             df.loc[len(df.index)] = [file, label, type, review]
-
-    # TODO: Create a single dataframe from the 4 IMBD directories (passed as argv[1]--argv[4]).
-    # HINT: You when you are trying to get the pos/neg value and the test/train value,
-    # you may want to use the `parts` attribute of a `Path` object
-
-
+    for filename in pos_test_files:
+        with open(filename, 'r', encoding='utf-8') as f:
+            file = filename
+            text = f.read()
+            review = cleanFileContents(text)
+            label = 'pos'
+            type = 'test'
+            df.loc[len(df.index)] = [file, label, type, review]
+    for filename in neg_test_files:
+        with open(filename, 'r', encoding='utf-8') as f:
+            file = filename
+            review = cleanFileContents(f)
+            label = 'neg'
+            type = 'test'
+            df.loc[len(df.index)] = [file, label, type, review]
     df.to_csv(new_filename)
-
-
-'''
-The function below should be called on a file name.
-It opens the file, reads its contents, and stores it in a variable.
-Then, it removes punctuation marks, and returns the "cleaned" text.
-'''
 
 
 def cleanFileContents(f):
