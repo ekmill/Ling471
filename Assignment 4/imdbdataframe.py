95743af17de08f4fdfd1687d6b8f4b7b1900dcec
ekmill <ekm20@uw.edu>
2022-05-22 18:44:54 -0700

    dataframe created

--- Assignment 4/imdbdataframe.py
+++ Assignment 4/imdbdataframe.py
@@ -16,41 +16,37 @@ def directory_list(argv):
     argv[2] = [f for f in train_neg.glob('*') if f.is_file]
     argv[3] = [f for f in test_pos.glob('*') if f.is_file]
     argv[4] = [f for f in test_neg.glob('*') if f.is_file]
-    return (argv[1, 2, 3, 4])
+    return argv[1, 2, 3, 4]
 
 # Constants:
 POS = 1
 NEG = 0
-
+n = 0
 
 def createDataFrame(argv):
     new_filename = "my_imdb_dataframe.csv"
-    for filename in argv:
+    column_names = ["file", "label", "type", "review"]
+    df = pd.DataFrame(data=argv[1, 4], columns=column_names)
+    for filename in directory_list(argv):
         with open(filename, 'r') as f:
-            File = filename
-            Review = f.read()
+            file = filename
+            text = f.read()
+            review = cleanFileContents(text)
+            if 'pos' in Path:
+                label = 'pos'
+            else:
+                label = 'neg'
+            if 'train' in Path:
+                type = 'train'
+            else:
+                type = 'test'
+            df.loc[len(df.index)] = [file, label, type, review]
+
     # TODO: Create a single dataframe from the 4 IMBD directories (passed as argv[1]--argv[4]).
-    df = pd.DataFrame(argv, columns=[File, Label, Type, Review])
-    # For example, "data" can be a LIST OF LISTS.
-    # In this case, each list is a set of column values, e.g. ["0_2.txt", "neg", "test", "Once again Mr Costner..."].
-    # That is, each list represents a single row in your data frame.
-    # You may use a different way of creating a dataframe so long as the result is accurate.
-    # TODO: Call the cleanFileContents() function on each file, as you are iterating over them.
-    data = []
-    # Your code here...
-    # Try to create a list of lists, for example, as illustrated above.
-    # Consider writing a separate function which takes a filename and returns a list representing the reivew vector.
-    # This will make your code here cleaner.
     # HINT: You when you are trying to get the pos/neg value and the test/train value,
     # you may want to use the `parts` attribute of a `Path` object
 
-    # Once you are done, the code below will only require modifications if your data variable is not a list of lists.
-    # Sample column names; you can use different ones if you prefer,
-    # but then make sure to make appropriate changes in assignment4_skeleton.py.
-    column_names = ["file", "label", "type", "review"]
-    # Sample way of creating a dataframe. This assumes that "data" is a LIST OF LISTS.
-    df = pd.DataFrame(data=data, columns=column_names)
-    # Saving to a file:
+
     df.to_csv(new_filename)
 
 
